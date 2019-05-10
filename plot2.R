b64628d5b009114e5ce0ef309297de67bb08acc6
rokbohinc86 <rok.bohinc86@gmail.com>
2019-05-10 16:12:58 +0200

    -Setting localtime so the names of weekdays come out in English -Minor corrections to plotlabels

--- plot2.R
+++ plot2.R
@@ -1,19 +1,22 @@
 library(data.table)
-
-#load data
-fdata="/home/rok/Edjucation/2019.3.28. Data_Science-Specialization/Exploratory Data Analysis/Week 1/Assighnment/ExData_Plotting1/data/household_power_consumption.txt"
-data <- fread(fdata, skip = 66637, nrows = 2880, na.strings = "?")
-names(data) <- names(fread(fdata, nrows = 1))
-
-# merge date and time and convert to posixct
 library(lubridate)
 library(dplyr)
-data <- data %>% mutate(DateTime = dmy_hms(paste(data$Date, data$Time))) %>% select(DateTime, Global_active_power:Sub_metering_3)
+
+# set localtime so the days of the week come out in english
+Sys.setlocale("LC_TIME", "C")
+
+#load data
+fdata="/home/rok/Edjucation/2019.3.28. Data_Science-Specialization/Exploratory Data Analysis/Week 1/ExData_Plotting1/data/household_power_consumption.txt"
+data <- fread(fdata, na.strings = "?") 
+# merge date and time and convert to posixct, selects the relevant columns, filters dates for 2007-02-01 and 2007-02-02
+data <- data %>% 
+        mutate(DateTime = dmy_hms(paste(data$Date, data$Time))) %>% 
+        select(DateTime, Global_active_power:Sub_metering_3) %>%
+        filter(DateTime>=ymd_hms("2007-02-01-00-00-00") & DateTime < ymd_hms("2007-02-03-00-00-00")) %>%
+        tbl_df
 
 # plot data
 par(mfrow = c(1, 1))
+png("plot2.png", width = 480, height = 480)
 plot(data$DateTime, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)") 
-# copy to file
-dev.copy(device = png, file = "plot2.png") 
 dev.off()
-#
--- plot3.R
+++ plot3.R
@@ -1,22 +1,26 @@
 library(data.table)
-
-# load data
-fdata="/home/rok/Edjucation/2019.3.28. Data_Science-Specialization/Exploratory Data Analysis/Week 1/Assighnment/ExData_Plotting1/data/household_power_consumption.txt"
-data <- fread(fdata, skip = 66637, nrows = 2880, na.strings = "?")
-names(data) <- names(fread(fdata, nrows = 1))
-
-# merge date and time and convert to posixct
 library(lubridate)
 library(dplyr)
-data <- data %>% mutate(DateTime = dmy_hms(paste(data$Date, data$Time))) %>% select(DateTime, Global_active_power:Sub_metering_3)
+
+# set localtime so the days of the week come out in english
+Sys.setlocale("LC_TIME", "C")
+
+#load data
+fdata="/home/rok/Edjucation/2019.3.28. Data_Science-Specialization/Exploratory Data Analysis/Week 1/ExData_Plotting1/data/household_power_consumption.txt"
+data <- fread(fdata, na.strings = "?") 
+# merge date and time and convert to posixct, selects the relevant columns, filters dates for 2007-02-01 and 2007-02-02
+data <- data %>% 
+        mutate(DateTime = dmy_hms(paste(data$Date, data$Time))) %>% 
+        select(DateTime, Global_active_power:Sub_metering_3) %>%
+        filter(DateTime>=ymd_hms("2007-02-01-00-00-00") & DateTime < ymd_hms("2007-02-03-00-00-00")) %>%
+        tbl_df
 
 # plot data and write to file
-png("plot3.png")
 par(mfrow = c(1, 1))
+png("plot3.png", width = 480, height = 480)
 plot(data$DateTime, data$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering") 
 points(data$DateTime, data$Sub_metering_1,col="black", type = "l") 
 points(data$DateTime, data$Sub_metering_2,col="red", type = "l")
 points(data$DateTime, data$Sub_metering_3,col="blue", type = "l")
 legend("topright", col = c("black","red", "blue"), lty=1, legend=names(data)[6:8])
 dev.off()
-#
